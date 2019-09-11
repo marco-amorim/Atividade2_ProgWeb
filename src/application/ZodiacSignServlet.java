f9b81fa4c6e933e40ae5941ae8e977721c061f62
Marco Amorim <marcoamorim95@hotmail.com>
2019-09-10 23:07:14 -0300

    Correções no código

--- src/application/ZodiacSignServlet.java
+++ src/application/ZodiacSignServlet.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.MonthDay;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -13,10 +14,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Professor, não sei porque mas esse código não funcionou no navegador interno
- * do Eclipse, no entanto no Google Chrome funcionou 100%
- */
 @WebServlet("/ZodiacSignServlet")
 public class ZodiacSignServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -37,10 +34,6 @@ public class ZodiacSignServlet extends HttpServlet {
 		out.println("</body></html>");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
@@ -57,39 +50,94 @@ public class ZodiacSignServlet extends HttpServlet {
 
 		out.println("</body></html>");
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String birthDate = request.getParameter("userBirthDate");
 		String userName = request.getParameter("userName");
 		Date parsedBirthDate = null;
-		MonthDay aries = MonthDay.of(03, 19);
-		MonthDay ariesLimit = MonthDay.of(04, 21);
 
 		try {
 			parsedBirthDate = sdf.parse(birthDate);
+			sdf.format(parsedBirthDate);
 
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 
-		MonthDay monthDayBirthDate = MonthDay.of(parsedBirthDate.getMonth(), parsedBirthDate.getDay());
+		LocalDate localDate = parsedBirthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		int month = localDate.getMonthValue();
+		int day = localDate.getDayOfMonth();
 
-		if (monthDayBirthDate.getMonthValue() == aries.getMonthValue()
-				&& monthDayBirthDate.getDayOfMonth() > aries.getDayOfMonth()
-				&& monthDayBirthDate.getDayOfMonth() < ariesLimit.getDayOfMonth()) {
-			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Aries!");
+		// Áries
+		if (month == 3 && day >= 21 || month == 4 && day <= 20) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Áries!");
 		}
 
-		// Professor, tentei fazer a lógica para a comparação das datas de várias
-		// maneiras,
-		// como o ano era irrelevante já que estavamos tentando achar o signo da pessoa,
-		// tentei
-		// fazer uma logica pegando somente o .getDay() e .getMonth() das datas, mas
-		// acabou que
-		// nao funcionou, entao descobri o MonthDay e vi que dava para ser usado, mas
-		// infelizmente
-		// nao tive tempo para continuar o projeto e entregar a tempo, vou continuar com
-		// as correções
-		// e se não conseguir resolver eu vejo com o Senhor na aula de quarta-feira.
+		// Touro
+		if (month == 4 && day >= 21 || month == 5 && day <= 20) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Touro!");
+		}
+
+		// Gêmeos
+		if (month == 5 && day >= 21 || month == 6 && day <= 20) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Gêmeos!");
+		}
+
+		// Câncer
+		if (month == 6 && day >= 21 || month == 7 && day <= 21) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Câncer!");
+		}
+
+		// Leão
+		if (month == 7 && day >= 22 || month == 8 && day <= 22) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Leão!");
+		}
+
+		// Virgem
+		if (month == 8 && day >= 23 || month == 9 && day <= 22) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Virgem!");
+		}
+
+		// Libra
+		if (month == 9 && day >= 23 || month == 10 && day <= 22) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Libra!");
+		}
+
+		// Escorpião
+		if (month == 10 && day >= 23 || month == 11 && day <= 21) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Escorpião!");
+		}
+
+		// Sagitário
+		if (month == 11 && day >= 22 || month == 12 && day <= 21) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Sagitário!");
+		}
+
+		// Capricórnio
+		if (month == 12 && day >= 22 || month == 1 && day <= 20) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Capricórnio!");
+		}
+
+		// Aquário
+		if (month == 1 && day >= 21 || month == 2 && day <= 19) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Aquário!");
+		}
+
+		// Peixes
+		if (month == 2 && day >= 20 || month == 3 && day <= 20) {
+
+			out.println("<br><br> Obrigado por fazer o teste " + userName + ", seu signo é de Peixes!");
+		}
 
 	}
 
